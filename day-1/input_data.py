341764b18237cd87dc73ba5d861913ae50a87ea2
codehath <codingfarhath@gmail.com>
2023-12-02 16:00:05 +0000

    # Refomatted input text file names

--- day-1/input_data.py
+++ day-1/input_data.py
@@ -1,15 +1,17 @@
+directory = "input/"
+
 # Day 1 Data
 input_day_01_sample_1 = None
-file_path = 'input/day_1.1_sample.txt'
+file_path = directory + 'input_day_01_sample_1.txt'
 with open(file_path, 'r') as file:
     input_day_01_sample_1 = file.readlines()
 
 input_day_01_sample_2 = None
-file_path = 'input/day_1.2_sample.txt'
+file_path = directory + 'input_day_01_sample_2.txt'
 with open(file_path, 'r') as file:
     input_day_01_sample_2 = file.readlines()
 
 input_day_01 = None
-file_path = 'input/day_1.txt'
+file_path = directory + 'input_day_01.txt'
 with open(file_path, 'r') as file:
     input_day_01 = file.readlines()
--- day-2/input_data.py
+++ day-2/input_data.py
@@ -1,10 +1,12 @@
+directory = "input/"
+
 # Day 2 Data
 input_day_02_sample = None
-file_path = 'input/day_2.1_sample.txt'
+file_path = directory + 'input_day_02_sample_1.txt'
 with open(file_path, 'r') as file:
-    input_day_02 = file.readlines()
+    input_day_02_sample = file.readlines()
 
 input_day_02 = None
-file_path = 'input/day_2.txt'
+file_path = directory + 'input_day_02.txt'
 with open(file_path, 'r') as file:
     input_day_02 = file.readlines()
